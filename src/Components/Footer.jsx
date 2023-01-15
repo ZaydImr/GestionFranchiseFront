cacaaaf3233281122ebc3e5acffab0139e2a3f9e
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-01-15 07:37:13 +0100

    Last chance :)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer style={{position:'fixed',width:'100vw',padding:'.5rem',textAlign:'center',bottom:0,left:0}}>
-            <a rel="noopener" href="https://www.linkedin.com/in/zaydimr/" target="_blank">Zayd EL IMRANI</a> 
+            <a rel="noreferrer" href="https://www.linkedin.com/in/zaydimr/" target="_blank">Zayd EL IMRANI</a> 
         </footer>
     )
 }
